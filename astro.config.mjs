2671edc08256b077a8242439f361c3de5955d51b
Malfrador <malfrador@gmail.com>
2024-10-27 01:39:31 +0200

    Move to Starlight for Docs

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Erethon Docs',
+			social: {
+				github: 'https://github.com/DRE2N',
+			},
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+		}),
+	],
+});
