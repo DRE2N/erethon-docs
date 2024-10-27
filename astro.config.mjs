7025b11ad526854973ff6a343e246a9416d5de35
Fyreum <luka.vogel@gmx.de>
2024-10-27 19:41:16 +0100

    Here we go again 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	base: '/docs',
-	integrations: [
-		starlight({
-			title: 'Erethon Docs',
-			social: {
-				github: 'https://github.com/DRE2N',
-			},
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-			],
-		}),
+  base: '/docs',
+
+  integrations: [
+      starlight({
+          title: 'Erethon Docs',
+          social: {
+              github: 'https://github.com/DRE2N',
+          },
+          customCss: [
+              // Relative path to your custom CSS file
+              './src/styles/custom.css',
+          ],
+      }),
 	],
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
